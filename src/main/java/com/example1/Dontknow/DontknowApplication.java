d3be63cd42b1183cc2aa65d6029e23ce929ed513
HanHenryOnlyFaceany <1264521752@qq.com>
2023-04-06 17:45:46 +0800

    2-2 init

---
+++ src/main/java/com/example1/Dontknow/DontknowApplication.java
@@ -0,0 +1,13 @@
+package com.example1.Dontknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontknowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontknowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example1/Dontknow/DontknowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example1.Dontknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontknowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
